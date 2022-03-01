6ffbd93606d54cb17dac7b80c7e6288dfd52e24d
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-01 19:07:32 +0500

    implementing splash screen api

--- app/src/main/java/xyz/teamgravity/splashscreenapi/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/splashscreenapi/MainActivity.kt
@@ -3,22 +3,39 @@ package xyz.teamgravity.splashscreenapi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import xyz.teamgravity.splashscreenapi.ui.theme.SplashScreenAPITheme
 
 class MainActivity : ComponentActivity() {
 
+    private val viewmodel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewmodel.loading.value
+            }
+        }
+
         setContent {
             SplashScreenAPITheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            modifier = Modifier.align(Alignment.Center),
+                            text = "Main Activity"
+                        )
+                    }
                 }
             }
         }
