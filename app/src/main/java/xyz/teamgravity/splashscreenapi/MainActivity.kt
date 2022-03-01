195e7c5cd05f7563f8726421985ccba9df25cdcb
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-01 18:39:20 +0500

    dependencies setup

--- app/src/main/java/xyz/teamgravity/splashscreenapi/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/splashscreenapi/MainActivity.kt
@@ -13,28 +13,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import xyz.teamgravity.splashscreenapi.ui.theme.SplashScreenAPITheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SplashScreenAPITheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SplashScreenAPITheme {
-        Greeting("Android")
     }
 }
